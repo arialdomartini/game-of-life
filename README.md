18be480e16700f11fcc3d249f9e3952a9f3098b9
Arialdo Martini <arialdomartini@gmail.com>
2015-01-26 09:31:40 +0100

    Un progetto vuoto, con un dummy test (verde)

---
+++ README.md
@@ -0,0 +1,4 @@
+Capoccione
+==========
+
+Il progetto per il Kata #1 di TicinoXP.
---
+++ src/Capoccione.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace Capoccione.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShouldPass()
+        {
+            "friends".Should().Be("friends");
+        }
+    }
+}
